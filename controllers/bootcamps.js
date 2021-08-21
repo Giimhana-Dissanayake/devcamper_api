d0bc811d33e23366ca821937f4ea07c196370ad8
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-08-21 19:42:22 +0530

    27.mongoose error handling 1

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,7 +10,9 @@ exports.getBootcamps = async (req, res, next) => {
     res
       .status(200)
       .json({ success: true, count: bootcamps.length, data: bootcamps });
-  } catch (err) {}
+  } catch (err) {
+    next(err);
+  }
 };
 
 // @desc     Get single bootcamp
@@ -23,17 +25,14 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
-      return new ErrorResponse(
-        `Bootcamp not found with id of ` + req.params.id,
-        404
+      return next(
+        new ErrorResponse(`Resource not found with id of ` + req.params.id, 404)
       );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    next(
-      new ErrorResponse(`Bootcamp not found with id of ` + req.params.id, 404)
-    );
+    next(err);
   }
 };
 
@@ -51,7 +50,7 @@ exports.createBootcamp = async (req, res, next) => {
       data: bootcamp,
     });
   } catch (err) {
-    res.status(500).json({ success: false });
+    next(err);
   }
 };
 
@@ -66,12 +65,14 @@ exports.updateBootcamp = async (req, res, next) => {
     });
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Resource not found with id of ` + req.params.id, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -83,11 +84,13 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Resource not found with id of ` + req.params.id, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: {} });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
